a17c85c3094f724d0ea6e634841e493c95c7c8db
abaddon101 <101891842+abaddon101@users.noreply.github.com>
2023-11-13 14:28:54 +0100

    creation table

--- src/Pages/EmployeeList.tsx
+++ src/Pages/EmployeeList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logoWealthHealth from "../Assets/logoWealthHealth.jpg";
+import Table from "react-bootstrap/Table";
 
 function EmployeeList() {
   return (
@@ -11,6 +12,56 @@ function EmployeeList() {
         </Link>
         <Link to="/CreateEmployee">CREATE EMPLOYEE</Link>
       </nav>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Start Date</th>
+            <th>Department</th>
+            <th>Date of Birth</th>
+            <th>Street</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
